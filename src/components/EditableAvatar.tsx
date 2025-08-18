5801c9f3d4047b348718d7032c85825232cdcf9c
totegamma <github@gammalab.net>
2025-08-18 14:20:11 +0900

    refactor

--- src/components/EditableAvatar.tsx
+++ src/components/EditableAvatar.tsx
@@ -1,24 +1,12 @@
 import { useEffect, useMemo, useRef } from 'react'
-import {
-    Texture,
-    Group,
-    AnimationMixer,
-    Mesh,
-    AnimationClip,
-    VectorKeyframeTrack,
-    AdditiveAnimationBlendMode,
-    QuaternionKeyframeTrack,
-    Euler,
-    Quaternion,
-    Object3D,
-    MeshBasicMaterial
-} from 'three'
+import { Texture, Group, AnimationMixer, Mesh, AdditiveAnimationBlendMode, Object3D, MeshBasicMaterial } from 'three'
 
 import { textureKeyMap, type AvatarParams, type TextureKind } from '../types'
 import { useGLTF } from '@react-three/drei'
 
 import { useFrame, useGraph } from '@react-three/fiber'
 import { FakeShadow } from './FakeShadow'
+import { createBaseAnimation } from '../util'
 
 type AvatarProps = {
     params: AvatarParams
@@ -33,99 +21,6 @@ export const EditableAvatar = ({ params, textures, editing, setSelected }: Avata
     const clone = useMemo(() => scene.clone(), [scene])
     const { nodes } = useGraph(clone)
     const mixer = useRef<AnimationMixer>(null)
-
-    const baseAnim = useMemo(() => {
-        // headSize
-        const track_headFrontScale = new VectorKeyframeTrack(
-            'Head_Front.scale',
-            [0],
-            [params.headSize, params.headSize, params.headSize]
-        )
-        const track_headBackScale = new VectorKeyframeTrack(
-            'Head_Back.scale',
-            [0],
-            [params.headSize, params.headSize, params.headSize]
-        )
-        // neckLength
-        const track_bodyPosition = new VectorKeyframeTrack(
-            'Body.position',
-            [0],
-            [0, -params.neckLength, params.headInFront ? -0.5 : 0.5]
-        )
-        // handSize
-        const track_leftHandScale = new VectorKeyframeTrack(
-            'LeftHand.scale',
-            [0],
-            [params.handSize, params.handSize, params.handSize]
-        )
-        const track_rightHandScale = new VectorKeyframeTrack(
-            'RightHand.scale',
-            [0],
-            [params.handSize, params.handSize, params.handSize]
-        )
-        // bodySize
-        const track_bodyFrontScale = new VectorKeyframeTrack(
-            'Body_Front.scale',
-            [0],
-            [params.bodySize, params.bodySize, params.bodySize]
-        )
-        const track_bodyBackScale = new VectorKeyframeTrack(
-            'Body_Back.scale',
-            [0],
-            [params.bodySize, params.bodySize, params.bodySize]
-        )
-        // tailSize
-        const track_tailFrontScale = new VectorKeyframeTrack(
-            'Tail_Front.scale',
-            [0],
-            [params.tailSize, params.tailSize, params.tailSize]
-        )
-        const track_tailBackScale = new VectorKeyframeTrack(
-            'Tail_Back.scale',
-            [0],
-            [params.tailSize, params.tailSize, params.tailSize]
-        )
-        // tailPosition
-        const track_tailPosition = new VectorKeyframeTrack('Tail.position', [0], [0, -params.tailPosition, 0])
-        // tailRotation
-        const rotation = new Quaternion().setFromEuler(new Euler(0, 0, params.tailRotation))
-        const track_tailRotation = new QuaternionKeyframeTrack('Tail.quaternion', [0], [...rotation.toArray()])
-        // legsSize
-        const track_legsScale = new VectorKeyframeTrack(
-            'Feet.scale',
-            [0],
-            [params.legsSize, params.legsSize, params.legsSize]
-        )
-        // legsDistance
-        const track_legsLeftDistance = new VectorKeyframeTrack('LeftFoot.position', [0], [params.legsDistance, 0, 0])
-        const track_legsRightDistance = new VectorKeyframeTrack('RightFoot.position', [0], [-params.legsDistance, 0, 0])
-        // legsDistanceFromBody
-        const track_legsPosition = new VectorKeyframeTrack(
-            'Feet.position',
-            [0],
-            [0, params.legsDistanceFromBody, params.legsInFront ? 0.5 : -0.5]
-        )
-
-        const clip = new AnimationClip('BaseAnimation', 1, [
-            track_headFrontScale,
-            track_headBackScale,
-            track_bodyPosition,
-            track_leftHandScale,
-            track_rightHandScale,
-            track_bodyFrontScale,
-            track_bodyBackScale,
-            track_tailFrontScale,
-            track_tailBackScale,
-            track_tailPosition,
-            track_tailRotation,
-            track_legsScale,
-            track_legsLeftDistance,
-            track_legsRightDistance,
-            track_legsPosition
-        ])
-        clip.blendMode = AdditiveAnimationBlendMode
-        return clip
-    }, [params])
 
     useEffect(() => {
         for (const key in nodes) {
@@ -168,7 +63,8 @@ export const EditableAvatar = ({ params, textures, editing, setSelected }: Avata
     }, [animations, mixer])
 
     useEffect(() => {
-        if (!mixer.current || !baseAnim) return
+        if (!mixer.current || !params) return
+        const baseAnim = createBaseAnimation(params)
         const action = mixer.current.clipAction(baseAnim)
         action.blendMode = AdditiveAnimationBlendMode
         action.play()
@@ -176,7 +72,7 @@ export const EditableAvatar = ({ params, textures, editing, setSelected }: Avata
         return () => {
             action.stop()
         }
-    }, [baseAnim, mixer])
+    }, [params, mixer])
 
     useEffect(() => {
         switch (editing) {
--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,16 @@
 import JSZip from 'jszip'
 import { BSON } from 'bson'
-import { Texture } from 'three'
+import {
+    AdditiveAnimationBlendMode,
+    AnimationClip,
+    Euler,
+    Quaternion,
+    QuaternionKeyframeTrack,
+    Texture,
+    VectorKeyframeTrack
+} from 'three'
 import brotliPromise from 'brotli-wasm'
-import type { AvatarManifest } from './types'
+import type { AvatarManifest, AvatarParams } from './types'
 
 export const handlePublish = async (
     thumbnail: Blob,
@@ -239,4 +247,97 @@ async function textureToPng(texture: Texture): Promise<{ blob: Blob; w: number; 
     }
 
     throw new Error('Unsupported texture.image type')
+}
+
+export const createBaseAnimation = (params: AvatarParams) => {
+    // headSize
+    const track_headFrontScale = new VectorKeyframeTrack(
+        'Head_Front.scale',
+        [0],
+        [params.headSize, params.headSize, params.headSize]
+    )
+    const track_headBackScale = new VectorKeyframeTrack(
+        'Head_Back.scale',
+        [0],
+        [params.headSize, params.headSize, params.headSize]
+    )
+    // neckLength
+    const track_bodyPosition = new VectorKeyframeTrack(
+        'Body.position',
+        [0],
+        [0, -params.neckLength, params.headInFront ? -0.5 : 0.5]
+    )
+    // handSize
+    const track_leftHandScale = new VectorKeyframeTrack(
+        'LeftHand.scale',
+        [0],
+        [params.handSize, params.handSize, params.handSize]
+    )
+    const track_rightHandScale = new VectorKeyframeTrack(
+        'RightHand.scale',
+        [0],
+        [params.handSize, params.handSize, params.handSize]
+    )
+    // bodySize
+    const track_bodyFrontScale = new VectorKeyframeTrack(
+        'Body_Front.scale',
+        [0],
+        [params.bodySize, params.bodySize, params.bodySize]
+    )
+    const track_bodyBackScale = new VectorKeyframeTrack(
+        'Body_Back.scale',
+        [0],
+        [params.bodySize, params.bodySize, params.bodySize]
+    )
+    // tailSize
+    const track_tailFrontScale = new VectorKeyframeTrack(
+        'Tail_Front.scale',
+        [0],
+        [params.tailSize, params.tailSize, params.tailSize]
+    )
+    const track_tailBackScale = new VectorKeyframeTrack(
+        'Tail_Back.scale',
+        [0],
+        [params.tailSize, params.tailSize, params.tailSize]
+    )
+    // tailPosition
+    const track_tailPosition = new VectorKeyframeTrack('Tail.position', [0], [0, -params.tailPosition, 0])
+    // tailRotation
+    const rotation = new Quaternion().setFromEuler(new Euler(0, 0, params.tailRotation))
+    const track_tailRotation = new QuaternionKeyframeTrack('Tail.quaternion', [0], [...rotation.toArray()])
+    // legsSize
+    const track_legsScale = new VectorKeyframeTrack(
+        'Feet.scale',
+        [0],
+        [params.legsSize, params.legsSize, params.legsSize]
+    )
+    // legsDistance
+    const track_legsLeftDistance = new VectorKeyframeTrack('LeftFoot.position', [0], [params.legsDistance, 0, 0])
+    const track_legsRightDistance = new VectorKeyframeTrack('RightFoot.position', [0], [-params.legsDistance, 0, 0])
+    // legsDistanceFromBody
+    const track_legsPosition = new VectorKeyframeTrack(
+        'Feet.position',
+        [0],
+        [0, params.legsDistanceFromBody, params.legsInFront ? 0.5 : -0.5]
+    )
+
+    const clip = new AnimationClip('BaseAnimation', 1, [
+        track_headFrontScale,
+        track_headBackScale,
+        track_bodyPosition,
+        track_leftHandScale,
+        track_rightHandScale,
+        track_bodyFrontScale,
+        track_bodyBackScale,
+        track_tailFrontScale,
+        track_tailBackScale,
+        track_tailPosition,
+        track_tailRotation,
+        track_legsScale,
+        track_legsLeftDistance,
+        track_legsRightDistance,
+        track_legsPosition
+    ])
+    clip.blendMode = AdditiveAnimationBlendMode
+    return clip
 }
